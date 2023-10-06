cbfac50af0c3a8429570b5dd57dba30672dc7c70
Santi Svirsky <santi@boat.media>
2023-10-06 10:27:11 -0300

    WIP tests

--- openpype/tests/test_standalone.py
+++ openpype/tests/test_standalone.py
@@ -1,2 +1,14 @@
 print("hello world")
-import openpype
+# import openpype
+
+import openpype.hosts.nuke
+
+from openpype.pipeline import install_host
+from openpype.hosts.nuke.api import NukeHost
+
+
+host = NukeHost()
+# install_host(host)
+
+print(dir(host))
+print("Success!")
