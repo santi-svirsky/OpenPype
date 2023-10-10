cf8071c3084456b47830d15793f6522a056626dc
Santi Svirsky <santi@boat.media>
2023-10-09 21:55:53 -0300

    bringing felix's code back

--- openpype/modules/puf_addons/gobbler/module.py
+++ openpype/modules/puf_addons/gobbler/module.py
@@ -4,13 +4,15 @@ import time
 import pyblish
 import click
 
+import gazu
+
 from openpype.modules import OpenPypeModule
 from openpype.pipeline.create import CreateContext
 # from abstract_publish import publish_version
 from openpype.client import get_project, get_asset_by_name
 from openpype.pipeline.context_tools import change_current_context
 
-from openpype.modules.puf_addons.gobbler.easy_publish import publish_version
+from openpype.modules.puf_addons.gobbler import easy_publish
 # from openpype.hosts.batchpublisher import BatchPublisherHost
 
 # from openpype.modules.kitsu.utils.credentials import (
@@ -37,9 +39,21 @@ class GobblerModule(OpenPypeModule):
     def cli(self, click_group):
         click_group.add_command(cli_main)
 
+    def get_global_environments(self):
+        """Get addon global environments.
+
+        Use standalone publisher for asset publishing
+        """
+        return {
+            "AVALON_APP_NAME": "standalonepublisher",
+            "AVALON_APP": "standalonepublisher",
+        }
+
+
+
 
 @click.group(GobblerModule.name,
-             help="Publish Workfile cli commands.")
+             help="Ingest mess from client.")
 def cli_main():
     pass
 
@@ -286,11 +300,11 @@ def go(project_name, directory=None):
 
     print(f"<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>> {os.environ.get('AVALON_PROJECT')}")
     # host = registered_host()
-    context = CreateContext(host)
+    # context = CreateContext(host)
 
 
 
-    publish_version(project_name,
+    easy_publish.publish_version(project_name,
                     asset_name,
                     task_name,
                     family_name,
@@ -304,6 +318,287 @@ def go(project_name, directory=None):
 
 
 
+@cli_main.command()
+@click.option("-p", "--project_name",
+              required=True,
+              envvar="AVALON_PROJECT",
+              help="Project name")
+@click.option("-d", "--directory",
+              required=False,
+              help="Directory to gobble")
+def test3(project_name):
+    # """Processes input directory. Walks through source, fuzzy-matches to OP assets and publishes."""
+    # print("You've triggered \"process_directory\" command.")
+    # DIRECTORY = '/sombrero/jobs/cse/in/ingest_test/'
+    # PROJECT = 'cse_test_056'
+    # project = PROJECT
+    # # pyblish.api.register_host("batch_publisher")
+
+
+    # # def board_publish(login, password, project, board_dir, upload_to_kitsu=False):
+    # #     """Synchronize openpype database from Zou sever database.
+
+    # #     Args:
+    # #         login (str): Kitsu user login
+    # #         password (str): Kitsu user password
+    # #         project (str): Project name
+    # #         board_dir (str): Path to storyboard shot movies directory
+    # #         upload_to_kitsu (bool): Upload directly to Kitsu
+    # #     """
+    # # validate_credentials(login, password)
+
+    # gazu.set_host("http://10.68.150.36/api")  # PROD
+    # gazu.log_in("admin@example.com", "mysecretpassword")
+
+
+    # # Fetch zou data0
+    # zou_project = gazu.project.get_project_by_name(project)
+
+    # # shots = {
+    # #     shot["name"]: shot
+    # #     for shot in gazu.shot.all_shots_for_project(zou_project)
+    # # }
+    # # print(shots)
+    # assets = {
+    #     asset["name"]: asset
+    #     for asset in gazu.asset.all_assets_for_project(zou_project)
+    # }
+    # task_type = gazu.task.get_task_type_by_name("Concept")
+    # task_status = gazu.task.get_task_status_by_short_name("wfa")
+
+
+    # # Register pyblish plugins
+    # pyblish.api.register_host("shell")
+    # openpype_path = Path(os.environ["OPENPYPE_REPOS_ROOT"])
+    # pyblish.api.register_plugin_path(
+    #     openpype_path.joinpath("openpype/plugins/publish").as_posix()
+    # )
+    # pyblish.api.register_plugin_path(
+    #     openpype_path.joinpath(
+    #         "openpype/hosts/standalonepublisher/plugins/publish"
+    #     ).as_posix()
+    # )
+
+    pass
+
+
+
+# === In the addon class ===
+
+# === Main script ===
+# @click.group(
+#     NormaalConfiguration.name, help="Normaal addon dynamic cli commands."
+# )
+# def cli_main():
+#     pass
+
+
+# @cli_main.command()
+# @click.option("--login", envvar="KITSU_LOGIN", help="Kitsu login")
+# @click.option(
+#     "--password", envvar="KITSU_PWD", help="Password for kitsu username"
+# )
+@click.option("-p", "--project", help="Project name")
+@click.option("-d", "--directory", help="Storyboard directory")
+# @click.option(
+#     "--upload-to-kitsu",
+#     help="Upload to kitsu directly within the script. Useful if regular publish system doesn't work",
+# )
+# def pupublish(login, password, project, board_dir, upload_to_kitsu=False):
+def pupublish(project, directory):
+    """Synchronize openpype database from Zou sever database.
+
+    Args:
+        login (str): Kitsu user login
+        password (str): Kitsu user password
+        project (str): Project name
+        board_dir (str): Path to storyboard shot movies directory
+        upload_to_kitsu (bool): Upload directly to Kitsu
+    """
+    from openpype.modules.kitsu.utils.credentials import (
+        # clear_credentials,
+        # load_credentials,
+        # save_credentials,
+        # set_credentials_envs,
+        validate_credentials,
+    )
+    # validate_credentials(login, password)
+
+    # Fetch zou data
+    zou_project = gazu.project.get_project_by_name(project)
+    board = gazu.task.get_task_type_by_name("Board")
+    real = gazu.task.get_task_status_by_name("Real")
+    shots = {
+        shot["name"]: shot
+        for shot in gazu.shot.all_shots_for_project(zou_project)
+    }
+    print(shots)
+    # Register pyblish plugins
+    pyblish.api.register_host("shell")
+    openpype_path = Path(os.environ["OPENPYPE_REPOS_ROOT"])
+    pyblish.api.register_plugin_path(
+        openpype_path.joinpath("openpype/plugins/publish").as_posix()
+    )
+    pyblish.api.register_plugin_path(
+        openpype_path.joinpath(
+            "openpype/hosts/standalonepublisher/plugins/publish"
+        ).as_posix()
+    )
+
+    # Set missing context keys
+    os.environ["AVALON_PROJECT"] = project
+    os.environ["AVALON_PROJECT_LOWER"] = project.lower()
+    os.environ["AVALON_TASK"] = "Board"
+
+    for filepath in Path(board_dir).iterdir():
+        # Get related zou shot
+        shot = shots.get(filepath.stem)
+        if not shot:
+            continue
+
+        # Get related zou task
+        task = gazu.task.get_task_by_name(shot, board)
+        if not task:
+            continue
+
+        # Upload to Kitsu
+        if upload_to_kitsu and task["task_status_id"] != real["id"]:
+            comment = gazu.task.add_comment(task, real)
+            preview = gazu.task.add_preview(
+                task,
+                comment,
+                preview_file_path=filepath,
+            )
+            gazu.task.set_main_preview(preview)
+
+        # Build shot appropriate name
+        seq = gazu.shot.get_sequence_from_shot(shot)
+        shot_name = "_".join([seq["episode_name"], seq["name"], filepath.stem])
+
+        # Build required pyblish data
+        os.environ["AVALON_ASSET"] = shot_name
+        context = pyblish.api.Context()
+        instance = context.create_instance(name=shot_name)
+        instance.data.update(
+            {
+                "family": "review",
+                "asset": shot_name,
+                "task": "Board",
+                "subset": "BoardReference",
+                "publish": True,
+                "active": True,
+                "source": filepath.as_posix(),
+            }
+        )
+
+        # Add representation
+        representation = {
+            "name": "mov",
+            "ext": "mov",
+            "preview": True,
+            "tags": ["review"],
+            "files": filepath.name,
+            "stagingDir": filepath.parent,
+        }
+        instance.data.setdefault("representations", [])
+        instance.data["representations"].append(representation)
+
+        # Publish to OP
+        context = pyblish.util.publish(context)
+
+        # Check published passed
+        for result in context.data["results"]:
+            if not result["success"]:
+                raise result.get("error")
+
+
+
+
+
+
+
+
+
+
+
+    # Set missing context keys
+    os.environ["AVALON_APP"] = 'Photoshop'
+    os.environ["AVALON_PROJECT"] = project
+    os.environ["AVALON_PROJECT_LOWER"] = project.lower()
+    os.environ["AVALON_TASK"] = "Concept"
+
+    for filepath in Path(DIRECTORY).iterdir():
+
+        # match_ratios = process.extract(filepath.stem, assets.keys, scorer=fuzz.token_sort_ratio)
+
+        # FUZZY MATCH filepath.stem vs assets.keys
+        best_match, _ = process.extractOne(filepath.stem, assets.keys(), scorer=fuzz.token_sort_ratio)
+        # print(best_match)
+
+        asset = assets.get(best_match)
+
+        # Get related zou task
+        print(task_type)
+        print(asset['id'])
+        task = gazu.task.get_task_by_name(asset, task_type)
+        print(task)
+        if not task:
+            continue
+
+
+
+        # Build required pyblish data
+        os.environ["AVALON_ASSET"] = asset['name']
+        context = pyblish.api.Context()
+        instance = context.create_instance(name=asset['name'])
+        instance.data.update(
+            {
+                "family": "review",
+                "asset": asset['name'],
+                "task": "Concept",
+                "subset": "background",
+                "publish": True,
+                "active": True,
+                "source": filepath.as_posix(),
+            }
+        )
+        # Add representation
+        ext = filepath.suffix.strip(".")
+        representation = {
+            "name": ext,
+            "ext": ext,
+            "preview": True,
+            "tags": ["review"],
+            "files": filepath.name,
+            "stagingDir": filepath.parent,
+        }
+        instance.data.setdefault("representations", [])
+        instance.data["representations"].append(representation)
+
+        # Publish to OP
+        context = pyblish.util.publish(context)
+
+        # Check published passed
+        for result in context.data["results"]:
+            if not result["success"]:
+                raise result.get("error")
+
+
+        # # Upload to Kitsu
+        # # if upload_to_kitsu and task["task_status_id"] != real["id"]:
+        # if ext in ["jpg", "png"]:
+        #     preview_file_path = filepath
+        # else:
+        #     print(f">>>>>>>>>>>>>>>>>>>>>>>>> need to find a jpeg for {filepath}")
+        #     print(instance.data)
+        #     preview_file_path = None  #
+        # comment = gazu.task.add_comment(task, task_status, f"Ingested from: {filepath}")
+        # preview = gazu.task.add_preview(
+        #     task,
+        #     comment,
+        #     preview_file_path=preview_file_path,
+        # )
+        # gazu.task.set_main_preview(preview)
 
 
 
